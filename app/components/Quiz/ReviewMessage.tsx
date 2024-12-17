96ac79f4bfb739c66922b84f285d4288529ecd86
sandhukirat23 <sandhukirat23@gmail.com>
2024-12-17 18:45:16 +1100

    fix: favicon & title

--- app/components/Quiz/ReviewMessage.tsx
+++
@@ -1,8 +0,0 @@
-export default function ReviewMessage() {
-    return (
-      <div style={{ color: 'red', marginTop: '20px' }}>
-        <p>Please review the course before continuing.</p>
-      </div>
-    );
-  }
-  
